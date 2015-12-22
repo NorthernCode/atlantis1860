55d087d44ce63bbf95eac43b2c790ebc690f4af5
Pekka Lammi <pekka.lammi@aalto.fi>
2015-12-23 00:04:33 +0200

    linted js correctly

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,13 @@
+   'use strict';
+
    $('document').ready(function() {
-           $('html, body').animate({
-                scrollTop: $("#bg").offset().top
-            }, 1500);
+    $('html, body').animate({
+        scrollTop: $('#bg').offset().top
+    }, 1500);
 
 
-             $(document).scroll(function(){
-             if(document.documentElement.clientHeight + 
-             $(document).scrollTop() >= document.body.offsetHeight )$(document).scrollTop(0);
-             });
-
-  }); 
+    $(document).scroll(function(){
+        if(document.documentElement.clientHeight +
+            $(document).scrollTop() >= document.body.offsetHeight ){$(document).scrollTop(0);
+        }});
+    });

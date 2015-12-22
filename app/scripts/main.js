1f9c61321be32e4d6fbceef0855109273263a0bf
Pekka Lammi <pekka.lammi@aalto.fi>
2015-12-22 23:57:03 +0200

    fresh start from the looping background

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+   $('document').ready(function() {
+           $('html, body').animate({
+                scrollTop: $("#bg").offset().top
+            }, 1500);
+
+
+             $(document).scroll(function(){
+             if(document.documentElement.clientHeight + 
+             $(document).scrollTop() >= document.body.offsetHeight )$(document).scrollTop(0);
+             });
+
+  }); 

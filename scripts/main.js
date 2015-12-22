d0b7a0671dcd84612d1f2f3fd5c97b4ba3cd1e52
Pekka Lammi <pekka.lammi@aalto.fi>
2015-12-23 01:57:34 +0200

    Update 2015-12-22T23:57:33.386Z

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";$("document").ready(function(){$("html, body").animate({scrollTop:$("#bg").offset().top},1500),$(document).scroll(function(){document.documentElement.clientHeight+$(document).scrollTop()>=document.body.offsetHeight&&$(document).scrollTop(0)})});

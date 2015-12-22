f9f52c070647272a5efe54d270d8600e4042596e
Pekka Lammi <pekka.lammi@aalto.fi>
2015-12-23 01:54:45 +0200

    cleaned up things a bit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,14 @@
    'use strict';
 
-   $('document').ready(function() {
+$('document').ready(function() {
     $('html, body').animate({
         scrollTop: $('#bg').offset().top
-    }, 1500);
+        }, 1500); //TODO: then show the upper bar
 
 
     $(document).scroll(function(){
-        if(document.documentElement.clientHeight +
-            $(document).scrollTop() >= document.body.offsetHeight ){$(document).scrollTop(0);
-        }});
+        if(document.documentElement.clientHeight + $(document).scrollTop() >= document.body.offsetHeight ){
+            $(document).scrollTop(0);
+        }
     });
+});

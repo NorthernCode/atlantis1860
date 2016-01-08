a2838ace201c0c10884b7f158fbb63a8dbaa2df8
Pekka Lammi <pekka.lammi@aalto.fi>
2016-01-08 11:57:51 +0200

    added content, parallax scrolling in progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,22 @@
-   'use strict';
+'use strict';
+
 
 $('document').ready(function() {
-    $('html, body').animate({
-        scrollTop: $('#bg').offset().top
-        }, 1500); //TODO: then show the upper bar
+
+	// $('.section').attr('data-stellar-ratio', '3');
+
+	$('#bg').attr('data-stellar-ratio', '0.5');
+
+	$.stellar();
+
+    // $('html, body').animate({
+    //     scrollTop: window.innerHeight / 2 //50vh
+    //     }, 1500); //TODO: then show the upper bar
 
 
-    $(document).scroll(function(){
-        if(document.documentElement.clientHeight + $(document).scrollTop() >= document.body.offsetHeight ){
-            $(document).scrollTop(0);
-        }
-    });
+    // $(document).scroll(function(){
+    //     if(document.documentElement.clientHeight + $(document).scrollTop() >= document.body.offsetHeight ){
+    //         $(document).scrollTop(0);
+    //     }
+    // });
 });

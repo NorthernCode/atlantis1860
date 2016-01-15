2bd4a8e5912f6c00864589ae6b8333d923df119a
Pekka Lammi <pekka.lammi@aalto.fi>
2016-01-15 16:31:03 +0100

    scrolling rework

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,10 +4,28 @@
 $('document').ready(function() {
 
 	// $('.section').attr('data-stellar-ratio', '3');
-
 	// $('#bg').attr('data-stellar-ratio', '0.5');
-
 	// $.stellar();
+
+    var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+
+    // build scenes
+    new ScrollMagic.Scene({triggerElement: "#parallax1"})
+                    .setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
+                    .addIndicators()
+                    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#parallax2"})
+                    .setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
+                    .addIndicators()
+                    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#parallax3"})
+                    .setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
+                    .addIndicators()
+                    .addTo(controller);
+
+
 
     // $('html, body').animate({
     //     scrollTop: window.innerHeight / 2 //50vh

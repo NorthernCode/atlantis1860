0b22154d2cef8184404b52ecbc8d6b1efeadf502
Pekka Lammi <pekka.lammi@aalto.fi>
2016-01-15 16:43:04 +0100

    remove all unnecesary js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,23 +7,23 @@ $('document').ready(function() {
 	// $('#bg').attr('data-stellar-ratio', '0.5');
 	// $.stellar();
 
-    var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+    // var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
 
-    // build scenes
-    new ScrollMagic.Scene({triggerElement: "#parallax1"})
-                    .setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
-                    .addIndicators()
-                    .addTo(controller);
+    // // build scenes
+    // new ScrollMagic.Scene({triggerElement: "#parallax1"})
+    //                 .setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
+    //                 .addIndicators()
+    //                 .addTo(controller);
 
-    new ScrollMagic.Scene({triggerElement: "#parallax2"})
-                    .setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
-                    .addIndicators()
-                    .addTo(controller);
+    // new ScrollMagic.Scene({triggerElement: "#parallax2"})
+    //                 .setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
+    //                 .addIndicators()
+    //                 .addTo(controller);
 
-    new ScrollMagic.Scene({triggerElement: "#parallax3"})
-                    .setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
-                    .addIndicators()
-                    .addTo(controller);
+    // new ScrollMagic.Scene({triggerElement: "#parallax3"})
+    //                 .setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
+    //                 .addIndicators()
+    //                 .addTo(controller);
 
 
 

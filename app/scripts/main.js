5c6cf9079d683cea9e0c619457431c3317e19a61
Pekka Lammi <pekka.lammi@aalto.fi>
2016-01-16 22:31:14 +0100

    optimized bg images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,39 +2,5 @@
 
 
 $('document').ready(function() {
-
-	// $('.section').attr('data-stellar-ratio', '3');
-	// $('#bg').attr('data-stellar-ratio', '0.5');
-	// $.stellar();
-
-    // var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
-
-    // // build scenes
-    // new ScrollMagic.Scene({triggerElement: "#parallax1"})
-    //                 .setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
-    //                 .addIndicators()
-    //                 .addTo(controller);
-
-    // new ScrollMagic.Scene({triggerElement: "#parallax2"})
-    //                 .setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
-    //                 .addIndicators()
-    //                 .addTo(controller);
-
-    // new ScrollMagic.Scene({triggerElement: "#parallax3"})
-    //                 .setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
-    //                 .addIndicators()
-    //                 .addTo(controller);
-
-
-
-    // $('html, body').animate({
-    //     scrollTop: window.innerHeight / 2 //50vh
-    //     }, 1500); //TODO: then show the upper bar
-
-
-    // $(document).scroll(function(){
-    //     if(document.documentElement.clientHeight + $(document).scrollTop() >= document.body.offsetHeight ){
-    //         $(document).scrollTop(0);
-    //     }
-    // });
+    $('#logo').animate({opacity: 'show'}, 3000, 'swing');
 });

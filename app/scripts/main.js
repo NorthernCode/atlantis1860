5b174cfdc7c279c31c1ed65b427fa66a48ef1b9f
Antti Virtanen <you@example.com>
2017-02-17 14:04:11 +0200

    2017 1st version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,5 +2,20 @@
 
 
 $('document').ready(function() {
-    $('#logo').animate({opacity: 'show'}, 3000, 'swing');
+    $('#fullpage').fullpage({
+        anchors: ['etusivu', 'liput', 'tarina', 'hahmot', 'tekijat', 'yhteistyossa'],
+        navigation: true,
+				navigationPosition: 'right',
+        menu: '#menu',
+        css3: true,
+        scrollingSpeed: 1000,
+        scrollOverflow: true
+    });
+    //$.fn.fullpage.setMouseWheelScrolling(false);
+    //$.fn.fullpage.setAllowScrolling(false);
+
+});
+
+$("#menu-mobile select").change(function() {
+  window.location = $(this).find("option:selected").val();
 });
